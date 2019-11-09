cf5714dd7082f40229d0a3f956cbd711793d9c56
hadrienj <hadrienjean@gmail.com>
2019-11-09 23:32:16 +0100

    Updates

---
+++ precache-manifest.bb8735b723ad533ff4f5f4f8d41451b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9866cfc30070ee64d28e15e48a7b8691",
+    "url": "/birdDetector/index.html"
+  },
+  {
+    "revision": "5e6ef9eaaf7ab4db9d45",
+    "url": "/birdDetector/static/css/main.7c17f9ab.chunk.css"
+  },
+  {
+    "revision": "348f47fa277a0d3a2953",
+    "url": "/birdDetector/static/js/2.6803d480.chunk.js"
+  },
+  {
+    "revision": "5e6ef9eaaf7ab4db9d45",
+    "url": "/birdDetector/static/js/main.e61bc4d8.chunk.js"
+  },
+  {
+    "revision": "22ddfa0724f677765f99",
+    "url": "/birdDetector/static/js/runtime-main.b63cc9b4.js"
+  }
+]);
